4ca0765dc513718064a73b400e23965501d0d1cd
jerry <jerrywangpico@gmail.com>
2016-07-08 16:58:21 -0400

    HomeScreen UI

--- app/src/main/java/abe/ppmd/StartUpScreen.java
+++ app/src/main/java/abe/ppmd/StartUpScreen.java
@@ -15,17 +15,7 @@ public class StartUpScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
